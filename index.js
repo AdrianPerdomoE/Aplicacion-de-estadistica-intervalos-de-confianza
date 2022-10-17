93a1205dc855eac2c7bb74a4c63925991cfaff23
AdrianPerdomoE <Perdomoecheverri@gmail.com>
2022-10-17 01:08:19 -0500

    Intervalos de confianza para la diferencia entre medias de poblaciones normales independientes

--- index.js
+++ index.js
@@ -34,5 +34,12 @@ function intervaloDiferenciaProporcionMuestraGrande(P1,P2,n1,n2,alfa){
     return {'Inferior':(P1-P2)-ZInv*((P1*(1-P1)/n1 +P2*(1-P2)/n2 )**(1/2)),'Superior':(P1-P2)+ZInv*((P1*(1-P1)/n1 +P2*(1-P2)/n2 )**(1/2))}
 }
 
+function intervaloDiferenciaMediaVarianzaConocidaMuestraGrande(X,Y,n1,n2,s1,s2,alfa){//s1 y s2 son la varianza, van al cuadrado si se conoce desviacion
+    let Z = new jsStats.NormalDistribution(0, 1);//Normal estandar
+    let alfaMedios = 1 - (alfa/2) // como el resultado esta en cola derecha, se considera el complemento
+    let ZInv = Z.invCumulativeProbability(alfaMedios)
+    return {'Inferior':(X-Y)-ZInv*((s1/n1+s2/n2)**(1/2)),'Superior':(X-Y)-ZInv*((s1/n1+s2/n2)**(1/2))}
+}
+
 
 
